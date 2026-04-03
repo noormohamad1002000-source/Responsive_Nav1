11d3b2e3524a74044d76a147f891b73bc3058e16
Noor Mohamad <noor.mohamad1002000@gmail.com>
2026-04-03 21:34:22 +0530

    fix: correct base path and dist folder

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/responsive_nav1/',    // ← add this line
+    base: '/Responsive_Nav1/',    // ← add this line
   plugins: [react(), tailwindcss(),],
 })
